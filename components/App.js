1b72104015517c2ea098c2b0ce35b038dad4810d
cekane <ckane144@gmail.com>
2016-07-09 01:42:44 -0400

    A whole Suite of Buttons

--- components/App.js
+++ components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Toggle } from './Toggle'
+import { Button } from './Button'
+import { Row } from './Row'
 
 export class App extends React.Component {
 
@@ -9,9 +12,28 @@ export class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>You're good to go</h1>
+				<Toggle />
+				<Row>	
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'medium' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'small' }/>
+				</Row>
+				<Row>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } color={ 'blue' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'medium' } color={ 'blue' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'small' } color={ 'blue' }/>
+				</Row>
+				<Row>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } color={ 'red' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'medium' } color={ 'red' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'small' }  color={ 'red' } />
+				</Row>
+				<Row>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } color={ 'yellow' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'medium' } color={ 'yellow' }/>
+					<Button text={ 'Click Me' } handleOnClick={ k=>{ console.log("You Clicked the buttons") } } size={ 'small' } color={ 'yellow' }/>
+				</Row>
 			</div>
-
 		);
 	}
 }
